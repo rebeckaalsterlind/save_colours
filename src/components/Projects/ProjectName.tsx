5a57037ec4a47ef2b820ae7012cdf3e190e22bc7
Johanna Bertils <johannabertils@hotmail.com>
2021-10-28 20:43:54 +0200

    creating mockdata and print out projectnames

--- src/components/Projects/ProjectName.tsx
+++ src/components/Projects/ProjectName.tsx
@@ -1,9 +1,16 @@
 import EditBtns from "../reuse/EditBtns"
+import data from "../../mockdata.json";
+
+console.log(data.projects);
 
 export default function ProjectName () {
+
     return (
         <div>
             <p>Propname</p>
+            {data.projects.map((project: any, index: number) => (
+                <h1 key={project.id}> {project.name}</h1>
+                ))}
             <EditBtns />
         </div>
     )
