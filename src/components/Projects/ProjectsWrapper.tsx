278292560e0cf6fb8c493d449ca8edc615a3024e
Johanna Bertils <johannabertils@hotmail.com>
2021-10-28 20:47:09 +0200

    map out project in right component

--- src/components/Projects/ProjectsWrapper.tsx
+++ src/components/Projects/ProjectsWrapper.tsx
@@ -1,9 +1,14 @@
 import Project from "./Project"
+import data from "../../mockdata.json";
+
+console.log(data.projects);
 
 export default function ProjectsWrapper () {
     return (
         <div>
-            // map Projects
+           {data.projects.map((project: any) => (
+                <h1 key={project.id}> {project.name}</h1>
+                ))}
             <Project />
         </div>
     )
