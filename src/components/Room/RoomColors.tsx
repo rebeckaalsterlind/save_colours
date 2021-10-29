3cb9a665c1ac467faeca1c306fc79be5ef5ed474
Lisa <ljwedin@gmail.com>
2021-10-29 09:45:44 +0200

    Installs bootstrap and edits login button to display a bootstrap style, also adds Bootstrap Docs link to ReadMe

--- src/components/Room/RoomColors.tsx
+++ src/components/Room/RoomColors.tsx
@@ -5,7 +5,7 @@ export default function RoomColors () {
     return (
         <div>
             // map colors
-            <Color /> // onclick show info, toggle show state
+            <Color color="color" /> // onclick show info, toggle show state
             <ColorInfo /> // toggled information about color
         </div>
     )
