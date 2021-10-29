b65ba4ea31fc06e3612c7dcf0b171e23741e54c0
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-10-29 14:51:37 +0200

    added user details to mockdata

--- src/components/Room/RoomColors.tsx
+++ src/components/Room/RoomColors.tsx
@@ -1,13 +1,14 @@
 import Color from "../reuse/Color"
 import ColorInfo from "../reuse/ColorInfo"
-    
 
 export default function RoomColors () {
     return (
         <div>
             // map colors
-            {/* <Color /> // onclick show info, toggle show state */}
+            <Color color="ba3056"/> // onclick show info, toggle show state
             <ColorInfo /> // toggled information about color
         </div>
     )
 }
+
+
