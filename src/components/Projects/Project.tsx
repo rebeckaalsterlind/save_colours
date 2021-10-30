c588d9f4fc0f8e64bb37ebee759c6e49356e5185
Johanna Bertils <johannabertils@hotmail.com>
2021-10-30 13:35:03 +0200

    change map of data according to new mockdata

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -3,7 +3,6 @@ import ProjectName from "./ProjectName"
 import Room from "../Room/Room"
 import data from "../../mockdata.json";
 
-console.log(data);
 
 interface Props {
 
@@ -16,13 +15,15 @@ interface State {
 
 export default function Project({ }: Props, state: State) {
 
+    console.log(data.projects);
+
     const [showProject, setShowProject] = useState(false);
     const [selectedproject, setSelectedproject] = useState();
 
     return (
         <div>
             {data.projects.map((project: any) => (
-                <h1 key={project.id} onClick={() => { setShowProject(!showProject); setSelectedproject(project); }}> {project.name}</h1>
+                <h1 key={project.projectId} onClick={() => { setShowProject(!showProject); setSelectedproject(project); }}> {project.projectName}</h1>
             ))}
             <ProjectName /> // prop: name
             // map room
