97d86f29756941603dcea7da2520c8d2b4676177
Johanna Bertils <johannabertils@hotmail.com>
2021-10-30 14:12:45 +0200

    showing projects connected to user only

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -2,33 +2,34 @@ import React, { useState } from 'react';
 import ProjectName from "./ProjectName"
 import Room from "../Room/Room"
 import data from "../../mockdata.json";
-
+import { store } from '../../store';
 
 interface Props {
-
 }
 
 interface State {
     showProject: boolean;
-    projectId: number;
+    selectedproject: object[];
 }
 
 export default function Project({ }: Props, state: State) {
-
-    console.log(data.projects);
 
     const [showProject, setShowProject] = useState(false);
     const [selectedproject, setSelectedproject] = useState();
 
     return (
         <div>
-            {data.projects.map((project: any) => (
-                <h1 key={project.projectId} onClick={() => { setShowProject(!showProject); setSelectedproject(project); }}> {project.projectName}</h1>
-            ))}
+
+            {store.getState().username === data.username && (
+                data.projects.map((project: any) => (
+                    <h1 key={project.projectId} onClick={() => { setShowProject(!showProject); setSelectedproject(project); }}> {project.projectName}</h1>
+                ))
+            )}
+
             <ProjectName /> // prop: name
             // map room
             {showProject && (
-            <Room project={selectedproject} />
+                <Room project={selectedproject} />
             )}
         </div>
     )
