7493095c9beefb09a59cb461b1d63d36ec4f89a6
Johanna Bertils <johannabertils@hotmail.com>
2021-11-03 16:46:06 +0100

    change colorinfo.tsx

--- src/components/reuse/ColorInfo.tsx
+++ src/components/reuse/ColorInfo.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-
 interface Props {
-    color: Object; 
+    color: Object
 }
 
 export default function ColorInfo({ color }: Props) {
+    console.log('color in colorInfo', color);
 
     return (
         <div>colorinfo ska visas som modal</div>
