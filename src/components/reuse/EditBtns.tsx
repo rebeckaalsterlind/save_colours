3a31216964bf2470d5148f77d889587fe3290593
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-11-04 12:21:18 +0100

    added edit btn icons, input focus in all forms, back function in forms

--- src/components/reuse/EditBtns.tsx
+++ src/components/reuse/EditBtns.tsx
@@ -1,9 +1,40 @@
 export default function EditBtns () {
+
+    const handleClick = (evt:React.MouseEvent) => {
+        const { target } = evt; 
+
+        switch((target as HTMLParagraphElement).id) {
+            case "share":
+                console.log('share project');
+            break;
+
+            case "edit":
+                console.log('edit project');
+            break;
+
+            case "delete":
+                console.log('delete project');
+            break;
+
+            default:
+            return null
+        }
+    }
+
     return (
         <aside style={{display:'inline'}}>
-            <p style={{display:'inline'}}>+</p>
-            <p style={{display:'inline'}}>O</p>
-            <p style={{display:'inline'}}>V</p>
+            &nbsp;&nbsp;<i 
+            id="share" 
+            onClick={handleClick} 
+            className="fas fa-share-alt">&nbsp; &nbsp;</i>
+            <i 
+            id="edit" 
+            onClick={handleClick} 
+            className="fas fa-edit">&nbsp; &nbsp;</i>
+            <i 
+            id="delete" 
+            onClick={handleClick} 
+            className="fas fa-trash-alt"></i>
         </aside>
     )
 }
