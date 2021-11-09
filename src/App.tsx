61353931c01224b300dec5ec0da19daf20b99dc1
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-11-09 11:04:01 +0100

    checking user logged in agains redux state instead of ls

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div className="App">
-      {!localStorage.getItem("userId")
+      {!store.getState().loggedIn
         ? <LogInPage />
         : <MainApp />
       }
