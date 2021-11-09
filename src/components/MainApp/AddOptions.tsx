4ff30c9b340728d17064f1f78502e3a22df136b0
Mats haby <mats.haby@live.com>
2021-11-09 11:21:24 +0100

    feat: add close/back button on addOptions and on add new project

--- src/components/MainApp/AddOptions.tsx
+++ src/components/MainApp/AddOptions.tsx
@@ -3,35 +3,46 @@ import { addColor, addProject } from '../../actions';
 import './addOptions.css';
 
 interface Props {
-  callback(hideComponent: boolean):void;
+    callback(hideComponent: boolean): void;
 }
 
-export default function AddOptions ({callback}:Props) {
-  
-    const handleClick = (evt:React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
+export default function AddOptions({ callback }: Props) {
+    const handleClick = (
+        evt: React.MouseEvent<HTMLParagraphElement, MouseEvent>
+    ) => {
         const { target } = evt;
 
         switch ((target as HTMLParagraphElement).id) {
-            case "wrapper":
-              callback(false);
-            break;
+            case 'wrapper':
+                callback(false);
+                break;
 
-            case "addColor":
-              store.dispatch(addColor(true))
-            break; 
+            case 'addColor':
+                store.dispatch(addColor(true));
+                break;
 
-            case "addProject":
-              store.dispatch(addProject(true))
-            break; 
+            case 'addProject':
+                store.dispatch(addProject(true));
+                break;
         }
-    }
+    };
 
     return (
         <div id="wrapper" className="modal-wrapper" onClick={handleClick}>
-          <div id="box" className="modal-box">
-            <p id="addColor" onClick={handleClick}>Lägg till ny färg</p>
-            <p id="addProject" onClick={handleClick}>Lägg till nytt projekt</p>
-          </div>
+            <div id="box" className="modal-box">
+                <button
+                    type="button"
+                    className="btn-close"
+                    aria-label="Close"
+                    onClick={() => callback(false)}
+                ></button>
+                <p id="addColor" onClick={handleClick}>
+                    Lägg till ny färg
+                </p>
+                <p id="addProject" onClick={handleClick}>
+                    Lägg till nytt projekt
+                </p>
+            </div>
         </div>
-    )
+    );
 }
