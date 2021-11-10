6809f59f2c801388e7b66b345bc8ea22c109f28d
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-11-10 11:39:18 +0100

    mapping chosen project & rooms to input in AddColor

--- src/components/Forms/AddColor.tsx
+++ src/components/Forms/AddColor.tsx
@@ -3,18 +3,21 @@ import { store as reduxStore } from '../../store';
 import { addColor as reduxAddColor } from '../../actions';
 
 interface State {
+    selectedProject: any;
     name: string;
     code: string;
     type: string;
     gloss: string;
     comment: string;
     store: string;
-    project: string;
-    room: string;
-    newColor: Object;
+    project: any;
+    room: any;
 }
 
 export default function AddColor({}, state: State) {
+    
+    const [selectedProject, setSelectedProject] = useState([]);
+
     const [name, setName] = useState('');
     const [code, setCode] = useState('');
     const [type, setType] = useState('');
@@ -24,7 +27,7 @@ export default function AddColor({}, state: State) {
 
     const [project, setProject] = useState('');
     const [room, setRoom] = useState('');
-    const [newColor, setNewColor] = useState({});
+    //const [newColor, setNewColor] = useState({});
 
     const inputRef: any = useRef(null);
 
@@ -33,11 +36,11 @@ export default function AddColor({}, state: State) {
         inputRef.current.focus();
     }, []);
 
+
     const handleSubmit = (evt: React.FormEvent) => {
         evt.preventDefault();
         const id = Math.floor(Math.random() * 10000) + 1;
-
-        setNewColor({
+        const newColor = {
             projectId: project,
             roomId: room,
             color: {
@@ -48,11 +51,36 @@ export default function AddColor({}, state: State) {
                 gloss: gloss,
                 comment: comment,
                 store: store,
-            },
-        });
+            }
+        };
+
+        console.log('newColor', newColor);
+        // setNewColor({
+        //     projectId: project,
+        //     roomId: room,
+        //     color: {
+        //         colorId: id,
+        //         colorName: name,
+        //         colorCode: code,
+        //         colorType: type,
+        //         gloss: gloss,
+        //         comment: comment,
+        //         store: store,
+        //     },
+        // });
 
         reduxStore.dispatch(reduxAddColor(false));
     };
+
+    useEffect(() => {
+
+       if(project !== '') {
+        const allProjects = reduxStore.getState().user.projects;
+        const foundProject = allProjects.find( (selectedProject:any) => selectedProject._id === project );
+        setSelectedProject(foundProject.rooms)
+        }
+
+    }, [project])
 
     return (
         <form className="d-grid col-6 mx-auto" onSubmit={handleSubmit}>
@@ -62,6 +90,7 @@ export default function AddColor({}, state: State) {
                 aria-label="Close"
                 onClick={() => reduxStore.dispatch(reduxAddColor(false))}
             ></button>
+
             <h5>Ny färg:</h5>
             <input
                 ref={inputRef}
@@ -69,22 +98,24 @@ export default function AddColor({}, state: State) {
                 type="text"
                 placeholder="Namn"
                 onChange={(evt) => setName(evt.target.value)}
-            />{' '}
+            />
             <br />
+
             <input
                 type="text"
                 className="form-control inputfield"
                 placeholder="NCS-kod"
                 onChange={(evt) => setCode(evt.target.value)}
-            />{' '}
+            />
             <br />
+            
             <select
                 className="form-select inputfield"
                 name="type"
                 id="type"
                 onChange={(evt) => setType(evt.target.value)}
             >
-                <option value="">Typ av Färg</option>
+                <option value="">--Typ av Färg--</option>
                 <option value="roof">Tak</option>
                 <option value="wall">Vägg</option>
                 <option value="floor">Golv</option>
@@ -92,6 +123,7 @@ export default function AddColor({}, state: State) {
                 <option value="floor">Övrigt</option>
             </select>
             <br />
+
             <select
                 className="form-select inputfield"
                 name="finish"
@@ -106,6 +138,7 @@ export default function AddColor({}, state: State) {
                 <option value="Other">Annan</option>
             </select>
             <br />
+
             <input
                 type="text"
                 className="form-control inputfield"
@@ -113,6 +146,7 @@ export default function AddColor({}, state: State) {
                 onChange={(evt) => setComment(evt.target.value)}
             />
             <br />
+
             <input
                 type="text"
                 className="form-control inputfield"
@@ -120,6 +154,7 @@ export default function AddColor({}, state: State) {
                 onChange={(evt) => setStore(evt.target.value)}
             />
             <br />
+
             <label htmlFor="project">Project:</label>
             <select
                 className="form-select inputfield"
@@ -127,9 +162,13 @@ export default function AddColor({}, state: State) {
                 id="project"
                 onChange={(evt) => setProject(evt.target.value)}
             >
-                {/* map projects here. value should be projectId, innerText should be projectName */}
+                  <option value="misc">--Välj projekt--</option>
+                {reduxStore.getState().user.projects.map((project: any) => (
+                    <option value={project._id}>{project.projectName}</option>))
+                }
             </select>
             <br />
+            
             <label htmlFor="room">Room:</label>
             <select
                 className="form-select inputfield"
@@ -137,9 +176,13 @@ export default function AddColor({}, state: State) {
                 id="room"
                 onChange={(evt) => setRoom(evt.target.value)}
             >
-                {/* map rooms here. value should be roomId, innerText should be roomName */}
+                <option value="misc">--Välj Rum--</option>
+                {selectedProject.map((room: any) => (
+                    <option value={room._id}>{room.roomName}</option>))
+                }
             </select>
             <br />
+
             <button className="btn btn-primary primary-btn">Spara färg</button>
         </form>
     );
